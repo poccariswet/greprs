62eb1865ce5f4dfc30e1abccc90d7be3a68300a3
poccariswet <s1240231@gmail.com>
2019-07-16 00:41:46 +0900

    add rust divided module

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    //    println!("{}", contents);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+extern crate greprs;
+
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let config = greprs::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    //   println!("Searching for {}", config.query);
+    //   println!("In file {}\n", config.filename);
+
+    if let Err(e) = greprs::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
